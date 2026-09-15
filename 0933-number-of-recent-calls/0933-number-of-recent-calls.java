f96c684ddac68ec172d46d2c8ed612d4e3971f9f
S UDAY KIRAN REDDY <udaykiranreddyseelam31@gmail.com>
2026-09-15 10:14:19 +0530

    Time: 22 ms (49.58%), Space: 59.6 MB (35.73%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,15 @@
+class RecentCounter {
+    private Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);
+        while (!q.isEmpty() && q.peek() < t - 3000) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
